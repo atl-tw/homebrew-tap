6679bda9ab350b2162c5832e1497de418cde1dc8
kebernet <rcooper@thoughtworks.com>
2024-04-21 23:28:50 +0000

    Updating caser to -1.0.71

--- Formula/caser.rb
+++ Formula/caser.rb
@@ -3,11 +3,11 @@ require "formula"
 class Caser < Formula
   desc 'A command line tool for converting between common multiword token formats'
   homepage 'https://github.com/atl-tw/caser'
-  url "https://github.com/atl-tw/caser/releases/download/1.0.70/caser-1.0.70-anyos.zip"
-  sha256 "7443177422b4cead15a588230bdfb817e64363ce6a4def5f77633edef62cdaf1"
-  version '1.0.70'
+  url "https://github.com/atl-tw/caser/releases/download/1.0.71/caser-1.0.71-anyos.zip"
+  sha256 "0e1085232bea6f9a885d7d95dabd0d4a125367f81823442c55b07e0ea715479b"
+  version '1.0.71'
 
-  # depends_on 'gnu-sed'
+  depends_on 'openjdk'
 
   def install
     bin.install 'bin/caser' => 'caser'
