b2e7be0e3766b277b47fba3cda58eb7ce2cb6688
kebernet <rcooper@thoughtworks.com>
2024-07-31 22:27:17 +0000

    Updating gha-notifier to -1.0.68

--- Casks/gha-notifier@1.0-SNAPSHOT.rb
+++ Casks/gha-notifier@1.0-SNAPSHOT.rb
@@ -3,14 +3,14 @@ cask "gha-notifier" do
 
   # Define URLs for different architectures
   if Hardware::CPU.arm?
-    url "https://github.com/atl-tw/gha-notifier/releases/download/1.0.67/gha-notifier-ARM_64_1.0.67.dmg"
-    sha256 "cfbaca8b943752dfa9a97aff1f3902f649cb5b94cbece55227bb70d44e3a1118"
+    url "https://github.com/atl-tw/gha-notifier/releases/download/1.0.68/gha-notifier-ARM_64_1.0.68.dmg"
+    sha256 "bcf7b32d05ddda90ac9a1944bf6690ae9cf6b6c608b9bbebacf66af05c050a4f"
   else
-    url "https://github.com/atl-tw/gha-notifier/releases/download/1.0.67/gha-notifier-x86_64_1.0.67.dmg"
-    sha256 :"cfbaca8b943752dfa9a97aff1f3902f649cb5b94cbece55227bb70d44e3a1118"
+    url "https://github.com/atl-tw/gha-notifier/releases/download/1.0.68/gha-notifier-x86_64_1.0.68.dmg"
+    sha256 "ceddbfcc2ab2a5df0bd48c91d33c941212a2f0a503effa7cba380d8d47c6da7d"
   end
 
-  name "GitHub Actions Notifier""
+  name "GitHub Actions Notifier"
   homepage "https://github.com/atl-tw/gha-notifier" # Replace with the application's homepage
 
   app "GitHub Actions Notifier.app"
