147c926d43fd3a06aa34cbe23f12d5f5005c11bd
kebernet <rcooper@thoughtworks.com>
2024-07-31 20:47:50 +0000

    Updating gha-notifier to -1.0.63

--- Formula/gha-notifier@1.0-SNAPSHOT.rb
+++ Formula/gha-notifier@1.0-SNAPSHOT.rb
@@ -1,10 +1,10 @@
 require "formula"
 
-class gha-notifier < Formula
+class GhaNotifier < Formula
   desc 'A desktop notifier for GitHub actions that delegates to the gh command line.'
   homepage 'https://github.com/atl-tw/gha-notifier'
-  url "https://github.com/atl-tw/gha-notifier/archive/refs/tags/1.0.62.zip"
-  sha256 "c523801dd75994b90399b54aace83f906118a6dd5128a61b5128d880f7a2f873"
+  url "https://github.com/atl-tw/gha-notifier/archive/refs/tags/1.0.63.zip"
+  sha256 "af47e6315b5504c2dde0a135cc8c75d1367a86a87e44f0e39fe805225b279847"
   version '1.0-SNAPSHOT'
 
   depends_on 'openjdk@17'
